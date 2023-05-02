7a56c82c92d6f3a111241ae1986155a9570c6ab5
Rasmus Persson <rasmus.persson9@hotmail.com>
2023-05-02 11:53:29 +0000

    Fix an error where a function was called inside a for loop on line 16

--- run.py
+++ run.py
@@ -1,19 +1,19 @@
 from random import randint
 
-HIDDEN_BOARD = [[''] * for x in range(8)]
-GUESS_BOARD = [[''] * for x in range(8)]
+HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
+GUESS_BOARD = [[' '] * 8 for x in range(8)]
 
 letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 
 """
-Define the board visuals, creating seperators through each iteration.
+Defining the board row and columns, creating seperators through each iteration.
 """
 def print_board(board):
-    print('   A B C D E F G H')
-    print('   +-+-+-+-+-+-+-+')
+    print('  A B C D E F G H')
+    print('  +-+-+-+-+-+-+-+')
     row_number = 1
-    for row in board():
+    for row in board:
         print("%d|%s" % (row_number, "|".join(row)))
         row_number += 1
 
