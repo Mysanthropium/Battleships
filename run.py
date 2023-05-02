557e0b4b27a0f21f8d471192cd981bd18e8c97dd
Rasmus Persson <rasmus.persson9@hotmail.com>
2023-05-02 10:37:12 +0000

    Define the playing board and implementing seperators

--- run.py
+++ run.py
@@ -1,13 +1,20 @@
-
+from random import randint
 
 HIDDEN_BOARD = [[''] * for x in range(8)]
 GUESS_BOARD = [[''] * for x in range(8)]
 
 letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
+"""
+Define the board visuals, creating seperators through each iteration.
+"""
 def print_board(board):
     print('   A B C D E F G H')
-    print('   ---------------')
+    print('   +-+-+-+-+-+-+-+')
+    row_number = 1
+    for row in board():
+        print("%d|%s" % (row_number, "|".join(row)))
+        row_number += 1
 
 def create_ships():
     pass
@@ -21,5 +28,7 @@ def count_hit_ships():
 create_ships()
 turns = 10
 #while turns > 0:
+
+
 
 
